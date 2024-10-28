2d27f276f427283f123eedc61b18983ed61ddbdc
Muller3014 <gmullfer@myuax.com>
2024-10-28 21:32:47 +0100

    Inclusion de Pi y area del circulo y modificaciones del CMakeLists.txt

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -2,6 +2,8 @@
 #include "Estudiante.h"
 #include "Persona.h"
 
+#define PI 3.14159
+#define AREA_CIRCULO(r) (PI * (r) * (r))
 
 int suma(int a, int b) {
     return a + b;
@@ -10,11 +12,14 @@ int varGlobal = 27;
 
 int main()
 {
+
     std::cout << "Hola mundo!" << std::endl;
 
+    std::cout <<"Pi es igual a: "<< PI << std::endl;
 
     std::cout << "La suma es: " << suma(2, 3) << std::endl;
     std::cout << "El valor de la variable global es: " << varGlobal << std::endl;
+    std::cout << "El valor del area del circulo utilizando la variable global como radio es: " << AREA_CIRCULO(varGlobal) << std::endl;
     std::cout << "La variable global + 3: " << suma(varGlobal, 3) << "\n" <<std::endl;
 
 
