4beab00550ac7b65ce29e3853e4a26f5019ee469
Muller3014 <gmullfer@myuax.com>
2024-10-28 20:43:52 +0100

    Mejora del main.cpp

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "Estudiante.h"
+#include "Persona.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int suma(int a, int b) {
+    return a + b;
+}
+int varGlobal = 27;
+
+int main()
+{
+    std::cout << "Hola mundo!" << std::endl;
+
+
+    std::cout << "La suma es: " << suma(2, 3) << std::endl;
+    std::cout << "El valor de la variable global es: " << varGlobal << std::endl;
+    std::cout << "La variable global + 3: " << suma(varGlobal, 3) << "\n" <<std::endl;
+
+
+    Persona persona1;
+
+    persona1.setNombre("John");
+    persona1.setEdad(20);
+    persona1.setGenero("Masculino");
+
+    std::cout << "Nombre: " << persona1.getNombre() << std::endl;
+    std::cout << "Edad: " << persona1.getEdad() << std::endl;
+    std::cout << "Genero: " << persona1.getGenero() << std::endl << std::endl;
+
+
+    Estudiante estudiante1;
+
+    estudiante1.setGrado("Ingenieria informatica");
+    estudiante1.setGenero("Femenino");
+    estudiante1.setNombre("Ana");
+    estudiante1.setEdad(22);
+
+    estudiante1.mostrarDetalles();
+
+
     return 0;
 }
